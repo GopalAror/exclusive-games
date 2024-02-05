0aeb369ac3a62ff472eca09f7d79b9fcba5231c6
GopalAror <punjabijanu586@gmail.com>
2024-02-05 09:41:16 +0530

    update

--- src/components/SlotMap.jsx
+++ src/components/SlotMap.jsx
@@ -7,7 +7,7 @@ const SlotMap = (props) => {
             <div className='hover:bg-slot p-[2px] rounded-[8px]'>
                 <div className='relative group rounded-[8px] overflow-hidden'>
                     <img className='w-full  relative z-0' src={props.img} alt="images" />
-                    <div className='bg-[#000000a3] overflow-hidden rounded-[8px] border-2 border-transparent absolute w-full z-[2] h-full flex justify-center group-hover:opacity-100 opacity-0 scale-[2] group-hover:scale-100 duration-500 items-center top-0'>
+                    <div className='bg-[#000000B2] overflow-hidden rounded-[8px] border-2 border-transparent absolute w-full z-[2] h-full flex justify-center group-hover:opacity-100 opacity-0 scale-[2] group-hover:scale-100 duration-500 items-center top-0'>
                         <Simplebtn simple="Jugar" />
                     </div>
                 </div>
