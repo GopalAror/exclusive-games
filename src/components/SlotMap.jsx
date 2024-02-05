c88c21ddf152faba08c61c06fa53a78146d5ef8e
GopalAror <punjabijanu586@gmail.com>
2024-02-05 09:31:41 +0530

    update

--- src/components/SlotMap.jsx
+++ src/components/SlotMap.jsx
@@ -4,10 +4,12 @@ import Simplebtn from './Simplebtn'
 const SlotMap = (props) => {
     return (
         <div data-aos={props.animate} className='sm:w-4/12 w-6/12 px-3 pt-6'>
-            <div className='relative overflow-hidden group rounded-[8px]'>
-                <img className='w-full  relative z-0' src={props.img} alt="images" />
-                <div className='bg-[#000000a3] border-2 border-transparent group-hover:border-[#6CE6A1] absolute w-full z-[2] h-full flex justify-center group-hover:opacity-100 opacity-0 scale-[2] group-hover:scale-100 duration-500 items-center top-0'>
-                    <Simplebtn simple="Jugar" />
+            <div className='hover:bg-slot p-[2px] rounded-[8px]'>
+                <div className='relative group rounded-[8px] overflow-hidden'>
+                    <img className='w-full  relative z-0' src={props.img} alt="images" />
+                    <div className='bg-[#000000a3] overflow-hidden rounded-[8px] border-2 border-transparent absolute w-full z-[2] h-full flex justify-center group-hover:opacity-100 opacity-0 scale-[2] group-hover:scale-100 duration-500 items-center top-0'>
+                        <Simplebtn simple="Jugar" />
+                    </div>
                 </div>
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,10 @@ module.exports = {
         'hover': 'linear-gradient(307deg, #51C8EF -13.72%, #7AF57A 102.02%)',
         'seven': "linear-gradient(306.99deg, rgba(81, 200, 239, 0.5) -13.72%, rgba(122, 245, 122, 0.5) 102.02%), linear-gradient(306.99deg, rgba(81, 200, 239, 0.04) -13.72%, rgba(122, 245, 122, 0.04) 102.02%)",
         'people': 'linear-gradient(306.99deg, rgba(81, 200, 239, 0.5) -13.72%, rgba(122, 245, 122, 0.5) 102.02%)',
-        'background': "linear-gradient(306.99deg, rgba(81, 200, 239, 0.5) -13.72%, rgba(122, 245, 122, 0.5) 102.02%), linear-gradient(306.99deg, rgba(81, 200, 239, 0.04) -13.72%, rgba(122, 245, 122, 0.04) 102.02%)"
+        'background': "linear-gradient(306.99deg, rgba(81, 200, 239, 0.5) -13.72%, rgba(122, 245, 122, 0.5) 102.02%), linear-gradient(306.99deg, rgba(81, 200, 239, 0.04) -13.72%, rgba(122, 245, 122, 0.04) 102.02%)",
+        "slot":"linear-gradient(306.99deg, #51C8EF -13.72%, #7AF57A 102.02%)",
+        
+        
       },
     },
     fontFamily: {
